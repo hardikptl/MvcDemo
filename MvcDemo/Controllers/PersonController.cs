80b71abbaa11b847abe2531680ef06a803e3fe69
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-17 17:31:24 -0500

    created list of person method and displaying values of list in to person view

--- MvcDemo/Controllers/PersonController.cs
+++ MvcDemo/Controllers/PersonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcDemo.Models;
 
 namespace MvcDemo.Controllers
 {
@@ -12,6 +13,16 @@ namespace MvcDemo.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ListPerson()
+        {
+            List<PersonModel> person = new List<PersonModel>();
+            person.Add(new PersonModel { FirstName = "hardik", LastName = "patel", Age = 35 });
+            person.Add(new PersonModel { FirstName = "hak", LastName = "pel", Age = 35 });
+            person.Add(new PersonModel { FirstName = "hdik", LastName = "pal", Age = 35 });
+
+            return View(person);
         }
     }
 }
