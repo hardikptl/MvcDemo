59fd7195d5b1510e595990aaa5c7eebadd439cb1
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-17 15:25:28 -0500

    created person model

---
+++ MvcDemo/Models/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; } = 0;
+
+        public bool IsAlive { get; set; } = true;
+
+    }
+
+}
